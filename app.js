483fc3686a38f773cc91b776631ecf3e23c5b153
carolpang <carolpang17@gmail.com>
2022-02-21 21:52:32 -0800

    added more validation

--- app.js
+++ app.js
@@ -37,7 +37,15 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username',
+      validate: githubNameInput => {
+        if (githubNameInput) {
+          return true;
+        } else {
+          console.log('Please enter your github name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -60,12 +68,28 @@ Add a New Project
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project?',
+      validate: projectNameInput => {
+        if (projectNameInput) {
+          return true;
+        } else {
+          console.log('Please enter your project name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: ProjectDescriptionInput => {
+        if (ProjectDescriptionInput) {
+          return true;
+        } else {
+          console.log('Please enter your project description!');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -76,7 +100,16 @@ Add a New Project
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: gitHublinkInput => {
+        if (gitHublinkInput) {
+          return true;
+        } else {
+          console.log('Please enter your gitHub link!');
+          return false;
+        }
+      }
+
     },
     {
       type: 'confirm',
